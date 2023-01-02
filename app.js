db14fdc6bc4ebbb6347c2a87e78c93c4c42032d9
StarJune <partis98@kakao.com>
2023-01-02 21:50:53 +0900

    DB 연결, 인증 미들웨어 구성

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ const express=require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const port = 8000;
 
-app.use('/auth', callback);
-app.listen(port);
+const port = 8080;
+
+const auth = require('./Middleware/auth');
+
+app.use(express.json());
+
+app.use('/auth', auth);
+
+mongoose.connect('mongodb+srv://root:test12345@cluster0.mnsjiqy.mongodb.net/?retryWrites=true&w=majority', {dbName: "Clon"})
+.then(() =>{
+    app.listen(port);
+    console.log('DB Connected Successful')
+})
+.catch(err =>{
+    console.log(err);
+})
