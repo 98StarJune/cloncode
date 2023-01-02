14af04c015557c52e7e3c9a366dfe25549544e0f
StarJune <partis98@kakao.com>
2023-01-02 22:14:01 +0900

    .

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const mongoose = require('mongoose');
 const port = 8080;
 
 const auth = require('./Middleware/auth');
+const dburl = require('./secret/Secret.json')
 
 app.use(express.json());
 
 app.use('/auth', auth);
 
-mongoose.connect('mongodb+srv://root:test12345@cluster0.mnsjiqy.mongodb.net/?retryWrites=true&w=majority', {dbName: "Clon"})
+mongoose.connect(dburl.dburl, {dbName: "Clon"})
 .then(() =>{
     app.listen(port);
     console.log('DB Connected Successful')
