955da4dcf4676c16da1d78693d9538081889f4f8
StarJune <partis98@kakao.com>
2023-01-03 21:10:14 +0900

    CORS 방지 로직 추가

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const port = 8080;
 
 const auth = require('./Middleware/auth');
 const dburl = require('./secret/secret.json')
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET');
+    next();
+})
 app.use(express.json());
 
 app.use('/auth', auth);
