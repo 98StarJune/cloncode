164145021006628cfcc004258236548147052662
StarJune <partis98@kakao.com>
2023-01-04 18:01:18 +0900

    로그인 처리 실패 수정

--- controller/auth/login.js
+++ controller/auth/login.js
@@ -13,15 +13,13 @@ module.exports.login = async (req, res, next) =>{
             return res.status(401).json({message : "존재하지 않는 회원입니다."});
         }
         else{
-            const error = new Error('에러가 발생했습니다.');
-            error.statusCode = 401;
-            error.root = "/control/auth/login/find"
-            throw error;
+            const token = await jwt.sign({
+                id : user._id.toString(),
+                time : new Date().toString()
+            }, secret.jwt, {expiresIn : '1h'});
+            res.status(200).json({token : token, message : "로그인에 성공했습니다."});
         }
-        const token = jwt.sign({
-            id : user._id.toString(),
-        }, secret.jwt, {expiresIn : '1h'});
-        res.status(200).json({token : token, message : "로그인에 성공했습니다."});
+
     }catch (err){
         errormessage(err, res);
     }
