5df18892fdc7f2743ef970c6af0c5860b8ec3c73
StarJune <partis98@kakao.com>
2023-01-06 18:53:14 +0900

    오류 발생 시 시간 표시 추가

--- controller/error.js
+++ controller/error.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 module.exports.errormessage = (err, res) => {
+    console.log(new Date());
     if(err.validation){
         return res.status(500).json({message : err.message, error : err.validation});
     }
     if(err.root){
+        console.log(new Date());
         console.log(err)
         console.log(err.root)
     }
