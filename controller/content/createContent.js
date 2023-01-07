93a04358dc5aeeff73419ae3e83de5485e48e320
StarJune <partis98@kakao.com>
2023-01-07 09:54:01 +0900

    게시글 추가 불가 현상 해결

--- controller/content/createContent.js
+++ controller/content/createContent.js
@@ -7,29 +7,33 @@ const {errormessage} = require("../error");
 const {validationResult} = require("express-validator");
 const {validation} = require("../validationError");
 
-module.exports.createContent = async (req, res, next) =>{
+module.exports.createContent = async (req, res, next) => {
     const id = req.userId;
+    if (id) {
+        console.log('로그인됨')
+    }
     const title = req.body.title;
     const content = req.body.content;
+    console.log(content)
     const tag = req.body.tag;
     const img = "/";
-    try{
-        const res_location = await Profile.findOne({id : id});
-        if(res_location){
+    try {
+        const res_location = await Profile.findOne({id: id});
+        if (res_location) {
             const location = res_location.location.location0;
-            const content = new Content({
-                title : title,
-                content : content,
-                img : img,
-                loaction : location[0],
-                tag : tag
+            const contents = new Content({
+                title: title,
+                content: content,
+                img: img,
+                loaction: location[0],
+                tag: tag
             })
-            const save_resault = await content.save();
-            if(save_resault){
-                res.status(201).json({message : "정상적으로 등록되었습니다."})
+            const save_resault = await contents.save();
+            if (save_resault) {
+                res.status(201).json({message: "정상적으로 등록되었습니다."})
             }
         }
-    }catch (err){
+    } catch (err) {
         errormessage(err, res);
     }
 }
