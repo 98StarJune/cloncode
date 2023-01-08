8c041023741b8cad5c7e26b802ab7a72f4fe7a7d
StarJune <partis98@kakao.com>
2023-01-08 13:32:29 +0900

    오류 테스트 코드

--- Middleware/content.js
+++ Middleware/content.js
@@ -8,8 +8,9 @@ const {createContent} = require('../controller/content/createContent');
 const {deleteContent} = require('../controller/content/deleteContent');
 const {findContentByKeyword, findAll, findDetail} = require('../controller/content/findContent');
 const {updateContent} = require('../controller/content/updateContent')
+const fileUpload = require('../Middleware/file')
 
-router.post('/create',jwtauth, createContent);
+router.post('/create',jwtauth, fileUpload.single('image'), createContent);
 router.post('/delete', jwtauth, deleteContent);
 router.post('/findkey', jwtauth, findContentByKeyword);
 router.post('/findall', jwtauth, findAll);
