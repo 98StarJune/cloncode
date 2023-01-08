c9021860d0e42247a73078f605b80d2cd3ffeba8
StarJune <partis98@kakao.com>
2023-01-08 10:53:50 +0900

    에러처리 일부 수정

--- controller/error.js
+++ controller/error.js
@@ -2,20 +2,20 @@ const express = require('express');
 
 module.exports.errormessage = (err, res) => {
     console.log(new Date());
-    if(err.validation){
-        return res.status(500).json({message : err.message, error : err.validation});
+    if (err.validation) {
+        return res.status(500).json({message: err.message, error: err.validation});
     }
-    if(err.message){
+    if (err.message) {
         console.log(err.message);
     }
-    if(err.root){
+    if (err.root) {
         console.log(new Date());
         console.log(err)
         console.log(err.root)
     }
-    if(err.statusCode){
-        res.status(err.statusCode).json({message : err.message});
-    }else{
-        res.json({message : err.message})
+    if (err.statusCode) {
+        res.status(err.statusCode).json({message: err.message});
+    } else {
+        res.json({message: err.message})
     }
 }
